ce1af0205d3560cbd662e48041c953fffbe4c7c1
Josema G <josemariagonzalez95@gmail.com>
2021-10-15 17:09:04 -0300

    Menú único. Configs solo para admins

--- app/helpers/auth.py
+++ app/helpers/auth.py
@@ -1,2 +1,7 @@
+from app.models.usuario_tiene_rol import usuario_tiene_rol
+
 def authenticated(session):
     return session.get("user")
+
+def isAdmin(anID):
+    return usuario_tiene_rol.find_by_id(anID) > 0
--- app/resources/configuracion.py
+++ app/resources/configuracion.py
@@ -1,90 +1,64 @@
 from sqlalchemy.sql.functions import current_timestamp
 from app.models.configuracion import Configuracion
-from flask import render_template, request , abort, redirect, url_for, flash
+from flask import render_template, request , abort, redirect, url_for, flash, session
 from app.db import db
+from app.helpers import auth
 
 def index():
-    return render_template("configuration.html")
+    if(auth.isAdmin(session.get("id"))):
+        return render_template("configuration.html")
+    else:
+        abort(401)
 
 def getConfigs():
     return Configuracion.getConfigs()
 
-def setColor1():
-    postdata = request.form
-    if(not postdata["color1"] == None):
-        Configuracion.getConfigs().color1Privada = postdata["color1"]
+def setConfigs():
+    if(auth.isAdmin(session.get("id"))):
+        postdata = request.form
+        if(postdata["color1"] != None):
+            Configuracion.getConfigs().color1Privada = postdata["color1"]
+        
+        if(not postdata["color2"] == None):
+            Configuracion.getConfigs().color2Privada = postdata["color2"]
+        
+        if(not postdata["color3"] == None):
+            Configuracion.getConfigs().color3Privada = postdata["color3"]
+
+        if(not postdata["max-elementos"] == None):
+            try:
+                newMaxElementos = int(postdata["max-elementos"])
+                Configuracion.getConfigs().maxElementos = newMaxElementos
+            except:
+                flash("Cantidad de elementos inválida")
+                abort(500)
+
+        if(not postdata["criterio"] == None):
+            Configuracion.getConfigs().criterio_orden = postdata["criterio"]
+
         try:
             db.session.commit()
-            flash("Paleta de colores actualizada")
+            flash("Configuración actualizada")
             return redirect(url_for("configuracion"))
         except:
-            return "Hubo un problema al actualizar el punto de encuento"
+            return "Erro al intentar configurar el sitio"
     else:
-        abort(404)
+        abort(401)
 
-
-def setColor2():
-    postdata = request.form
-    if(not postdata["color2"] == None):
-        Configuracion.getConfigs().color2Privada = postdata["color2"]
-        try:
-            db.session.commit()
-            flash("Paleta de colores actualizada")
-            return redirect(url_for("configuracion"))
-        except:
-            return "Hubo un problema al actualizar el punto de encuento"
-    else:
-        abort(404)
-
-def setColor3():
-    postdata = request.form
-    if(not postdata["color3"] == None):
-        Configuracion.getConfigs().color3Privada = postdata["color3"]
-        try:
-            db.session.commit()
-            flash("Paleta de colores actualizada")
-            return redirect(url_for("configuracion"))
-        except:
-            return "Hubo un problema al actualizar el punto de encuento"
-    else:
-        abort(404)
-
-def setMaxElementos():
-    postdata = request.form
-    if(not postdata["max-elementos"] == None):
-        Configuracion.getConfigs().maxElementos = postdata["max-elementos"]
-        try:
-            db.session.commit()
-            flash("Cantidad de elementos configurados.")
-            return redirect(url_for("configuracion"))
-        except Exception as e:
-            return str(e)
-    else:
-        abort(404)
-
-def setCriterioOrden():
-    postdata = request.form
-    if(not postdata["criterio"] == None):
-        Configuracion.getConfigs().criterio_orden = postdata["criterio"]
-        try:
-            db.session.commit()
-            flash("Criterio de ordenación actualizado.")
-            return redirect(url_for("configuracion"))
-        except:
-            return "Hubo un problema al actualizar el criterio de ordenación"
-    else:
-        abort(404)
 
 def toggleMaintenance():
-    current_mantenimiento = Configuracion.getConfigs().sitio_en_mantenimiento
-    Configuracion.getConfigs().sitio_en_mantenimiento = not (current_mantenimiento)
-    try:
-        db.session.commit()
-        if(current_mantenimiento):
-            flash("Sitio publicado. Estamos en vivo")
-        else:
-           flash("Sitio puesto en mantenimiento") 
-        
-        return redirect(url_for("configuracion"))
-    except:
-        return "Erro al intentar configurar el sitio"
+    if(auth.isAdmin(session.get("id"))):
+        current_mantenimiento = Configuracion.getConfigs().sitio_en_mantenimiento
+        Configuracion.getConfigs().sitio_en_mantenimiento = not (current_mantenimiento)
+        try:
+            db.session.commit()
+            if(current_mantenimiento):
+                flash("Sitio publicado. Estamos en vivo")
+            else:
+                flash("Sitio puesto en mantenimiento")
+
+            return redirect(url_for("configuracion"))
+        except:
+            return "Erro al intentar configurar el sitio"
+    else:
+        abort(401)
