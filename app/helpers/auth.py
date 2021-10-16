2cdfe0b52f29468904a69630ee0167fa50f5d605
Josema G <josemariagonzalez95@gmail.com>
2021-10-16 11:03:18 -0300

    Se agregó la funcionalidad en feature/permisosnuevo. Para verificar los permisos de una persona se puede usar el funcion hasPermission en helpers/auth

--- app/helpers/auth.py
+++ app/helpers/auth.py
@@ -1,7 +1,11 @@
 from app.models.usuario_tiene_rol import usuario_tiene_rol
+from app.models.permiso import Permiso
 
 def authenticated(session):
     return session.get("user")
 
 def isAdmin(anID):
     return usuario_tiene_rol.find_by_id(anID) > 0
+
+def hasPermission(aPermission, aUserID):
+    return Permiso.has_permission(aPermission, aUserID) 
