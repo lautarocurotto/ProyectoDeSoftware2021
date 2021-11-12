b3d07f840f93b7e1ba871e934688f6b5672470a0
Felipe <felipe2001.b@outlook.com>
2021-11-12 19:45:09 -0300

    agregue la relacion entre rol y permiso

--- app/models/rol_tiene_permiso.py
+++ app/models/rol_tiene_permiso.py
@@ -1,5 +1,6 @@
 from re import A
 from sqlalchemy.orm import load_only
+from sqlalchemy.sql.schema import ForeignKey
 from sqlalchemy.sql.sqltypes import Date
 from app.db import db
 from sqlalchemy import Column,Integer,String,Boolean,DateTime,exists
@@ -7,8 +8,9 @@ from sqlalchemy import Column,Integer,String,Boolean,DateTime,exists
 
 class rol_tiene_permiso(db.Model):
     __tablename__="rol_tiene_permiso"
-    rol_id=Column(Integer,primary_key=True)
-    permiso_id=Column(Integer,primary_key=True)
+    rol_id=Column(ForeignKey("Rol.id"),primary_key=True)
+    permiso_id=Column(ForeignKey("Permiso.id"),primary_key=True)
+
 
 
 def __init__(self,rol_id=None,permiso_id=None):
--- app/resources/usuario.py
+++ app/resources/usuario.py
@@ -238,6 +238,7 @@ def updatePerfil(id):
 
 
 
+
     
     
 
