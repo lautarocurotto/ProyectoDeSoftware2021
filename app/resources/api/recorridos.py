2c0a9dd57b6051c9088b8f74f2a01e0cd8765e09
Gaston Ciancio <gastonciancio4@gmail.com>
2021-11-25 13:09:59 -0300

    api recorridos lista

--- app/resources/api/recorridos.py
+++ app/resources/api/recorridos.py
@@ -10,19 +10,16 @@ recorridos_evacuacion_api= Blueprint("puntos-encuentro", __name__, url_prefix="/
 @recorridos_evacuacion_api.get("/")
 def index():
     max_elementos=Configuracion.get_configs().maxElementos
-    pagina=int(request.args.get("page",1))
-    recorridos_aux=Recorrido.query.paginate(page=1,per_page=max_elementos)
-    if(Recorrido.cantidad_recorridos()==0):
-        data={"mensaje":"No hay recorridos de evacuacion en el sistema"}
-        return jsonify(data),404
-    if(pagina<=recorridos_aux.pages):
-        recorridos_rows=Recorrido.query.paginate(page=pagina,per_page=max_elementos)
-        data= {
+    try:
+        pagina=int(request.args.get("page",1))
+    except:
+        pagina=1
+    recorridos_rows=Recorrido.query.paginate(page=pagina,per_page=max_elementos,error_out=False)
+    data= {
             "recorridos":[recorrido.as_dict() for recorrido in recorridos_rows.items],
             "page":recorridos_rows.page,
             "total":recorridos_rows.total,
         }
-        return jsonify(data),200
-    else:
-        data={"mensaje":"Mandaste mal el numero de pagina"}
-        return jsonify(data),400
+    return jsonify(data),200
+  
+     
