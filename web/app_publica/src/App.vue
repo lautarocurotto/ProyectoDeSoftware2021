d564737f92ca426f64226f00f85fe4e1e63faf38
Gaston Ciancio <gastonciancio4@gmail.com>
2021-11-26 20:49:37 -0300

    comienzo exitoso

--- web/app_publica/src/App.vue
+++ web/app_publica/src/App.vue
@@ -2,6 +2,7 @@
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
+    <router-link to="/mapaRecorridosPuntos">Mapa de recorridos y puntos de evacuacion</router-link>
   </div>
   <router-view/>
 </template>
