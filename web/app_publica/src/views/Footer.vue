b9d64da5599c2dddabacb06c3fe60d89ddfae147
Josema <josemariagonzalez95@gmail.com>
2021-11-29 15:53:49 -0300

    Home

---
+++ web/app_publica/src/views/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="container">
+      <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4">
+        <p class="col-md-4 mb-0 text-muted">&copy; Grupo #40</p>
+    
+        <a href="/" class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+          <img src="../assets/laplatacorazon.png" alt="" height="40" width="50">
+        </a>
+  
+      </footer>
+    </div>
+</template>
+
+<script>
+export default {
+    name : 'Footer'
+}
+</script>
---
+++ web/app_publica/src/views/NavBar.vue
@@ -0,0 +1,26 @@
+<template>
+    <nav>
+        <div class="container">
+            <header class="d-flex flex-wrap justify-content-center py-3 mb-4">
+              <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+                <img src="../assets/laplatacorazon.png" height="40" width="50" alt="">
+                <span class="fs-4">Zonas Inundables</span>
+              </a>
+        
+              <ul class="nav">
+                <!-- <li class="nav-item"><a href="#" class="nav-link active" aria-current="page">Home</a></li> -->
+                <li class="nav-item"><a href="#denunciar" class="nav-link">Denunciar</a></li>
+                <li class="nav-item">
+                  <a href="#" class="btn btn-outline-primary"><i class="fas fa-user-plus"></i> Registrarse</a>
+                </li>
+              </ul>
+            </header>
+          </div>
+    </nav>
+</template>
+
+<script>
+export default {
+  name: 'NavBar'
+}
+</script>
